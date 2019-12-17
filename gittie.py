8bb352fb75634543fdb6fa973b2d2d669130b9e4
Lidia Krawczyk <krawczyklid@gmail.com>
2019-12-17 11:13:38 +0100

    Greetings changes

--- gittie.py
+++ gittie.py
@@ -1,11 +1,12 @@
-print('Hello!')
-print('Hello gittie!')
+greetings = (input('Hello! What is your name?'))
+print('Hello {}!'.format(greetings))
 
 answer = input('Do you want to hear a joke?')
-if answer = 'y':
+if answer == 'y':
     print('przychodzi baba do lekarza a lekarz tez baba')
 else:
     print('bye!')
+
 a = 56
 b = 78
 c = a*b
