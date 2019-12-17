df8e75d36b7383e1d246093c4aecd6fb4f853132
Ola <piskorz.aleksandra@gmail.com>
2019-12-17 11:23:30 +0100

    bye bye bye

--- gittie.py
+++ gittie.py
@@ -7,7 +7,7 @@ answer = input('Do you want to hear a joke?')
 if answer == 'y':
     print('przychodzi baba do lekarza a lekarz tez baba')
 else:
-    print('bye!')
+    print('bye! bye beukzzLNFkfnz')
 
 a = 56
 b = 78
