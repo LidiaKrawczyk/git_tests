c147c8ee2598b36000f3deefb416c1b491725147
Lidia Krawczyk <krawczyklid@gmail.com>
2019-12-17 11:27:33 +0100

    While finction

--- gittie.py
+++ gittie.py
@@ -1,7 +1,13 @@
 print('Hello!')
 print('Hello gittie! I am trying to learn GIT!')
+
 greetings = (input('Hello! What is your name?'))
 print('Hello {}!'.format(greetings))
+
+index = 0
+while index < 4:
+    print("donkey")
+    index += 1
 
 answer = input('Do you want to hear a joke?')
 if answer == 'y':
