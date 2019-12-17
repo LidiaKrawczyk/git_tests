1f6efacccbe7fe773ee97922cbc9e5d453086357
Ola <piskorz.aleksandra@gmail.com>
2019-12-17 11:04:29 +0100

    joke

--- gittie.py
+++ gittie.py
@@ -1,2 +1,8 @@
 print('Hello!')
 print('Hello gittie!')
+
+answer = input('Do you want to hear a joke?')
+if answer = 'y':
+    print('przychodzi baba do lekarza a lekarz tez baba')
+else:
+    print('bye!')
